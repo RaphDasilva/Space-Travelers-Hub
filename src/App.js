fecfed6f87bd510fb8ffc8d132334d67d59cdd0d
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-03-07 15:53:35 +0530

    Update the index.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './styles/App.css';
 import './styles/header.css';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Missions from './components/Missions';
+import Rockets from './components/Rockets';
+import Profile from './components/Profile';
 
 function App() {
   return (
-    <>
+    <Router>
       <div className="container">
         <Header />
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="/Missions" element={<Missions />} />
+          <Route path="/Profile" element={<Profile />} />
+        </Routes>
 
       </div>
-    </>
+    </Router>
   );
 }
 
