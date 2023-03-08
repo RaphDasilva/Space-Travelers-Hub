734bced8efc985f2bddda168cae2388e68c3dede
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-03-08 17:31:47 +0530

    Implement joinMission and leaveMission action.

--- src/components/DisplayMission.js
+++ src/components/DisplayMission.js
@@ -4,7 +4,7 @@ import styles from '../styles/DisplayMission.module.css';
 import { getMission, missionActions } from '../redux/mission/missionSlice';
 
 const DisplayMission = () => {
-  const { mission, status, id } = useSelector((store) => store.mission);
+  const { mission, status } = useSelector((store) => store.mission);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -25,39 +25,47 @@ const DisplayMission = () => {
     dispatch(missionActions.joinMission(id));
   };
 
+  const leaveMissionHandler = (e) => {
+    const { id } = e.target.dataset;
+    dispatch(missionActions.leavingMission(id));
+  };
+
   return (
     <>
       <div>{outPut}</div>
-      {
-        mission ? (
-          mission.map((item) => (
-            <div className={styles.missionItems} key={item.mission_id}>
-              <div className={styles.missionHead}>
-                <h3>{item.mission_name}</h3>
-              </div>
-              <div className={styles.missionInfo}>
-                <p>
-                  {item.description}
-                </p>
-              </div>
-              <div className={styles.buttons}>
-                <button type="button" className={styles.memberBtn}>
-                  NOT A MEMBER
-                </button>
-              </div>
-              <div className={styles.buttons}>
-                <button type="button" data-id={id} className={styles.joinMissionBtn} onClick={joinMissionHandler}>
+      {mission ? (
+        mission.map((item) => (
+          <div className={styles.missionItems} key={item.mission_id}>
+            <div className={styles.missionHead}>
+              <h3>{item.mission_name}</h3>
+            </div>
+            <div className={styles.missionInfo}>
+              <p>
+                {item.description}
+              </p>
+            </div>
+            <div className={styles.buttons}>
+              {!item.reserved ? (<button type="button" className={styles.memberBtn}>Not A Member</button>
+              ) : (<button type="button" className={styles.active}>Active Member</button>)}
+            </div>
+            <div className={styles.buttons}>
+              {!item.reserved ? (
+                <button data-id={item.mission_id} onClick={joinMissionHandler} type="button" className={styles.joinMissionBtn}>
                   Join Mission
                 </button>
-              </div>
+              ) : (
+                <button data-id={item.mission_id} onClick={leaveMissionHandler} type="button" className={styles.leavingMission}>
+                  Leave Mission
+                </button>
+              )}
             </div>
-          ))
-        ) : (
-          <div className="loading">
-            <h5>Loading....</h5>
           </div>
-        )
-      }
+        ))
+      ) : (
+        <div className="loading">
+          <h5>Loading....</h5>
+        </div>
+      )}
     </>
   );
 };
