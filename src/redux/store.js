f9cf6a0d3d7638c54f3c74d7ca14b2d03f67a2f7
daniel matama <danmatama2020@gmail.com>
2023-03-08 12:28:05 +0300

    Create home-page

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionReducer from './mission/missionSlice';
+import rocketsReducer from './rockets/rockets';
 
 const store = configureStore({
   reducer: {
-    mission: missionReducer,
+    mission: missionReducer, rocketsReducer,
   },
 });
 
